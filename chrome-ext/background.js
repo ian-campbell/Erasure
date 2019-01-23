4a2f3a84453b551f99e53f91b4277c69b28f111f
Ian Campbell <ian.campbell87@gmail.com>
2019-01-23 18:43:20 -0500

    Delete page_action logic in background.js

--- chrome-ext/background.js
+++ chrome-ext/background.js
@@ -2,18 +2,3 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-'use strict';
-
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({ color: '#3aa757' }, function() {
-        console.log("The color is green.");
-    });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: { urlContains: 'www.youtube.com' },
-            })],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-    });
-});

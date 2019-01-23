f1f5d57297e3d500f06ae8d56a17eccc1a53fa52
Ian Campbell <ian.campbell87@gmail.com>
2019-01-22 23:32:33 -0500

    Updated list indices for button clicks

--- youtubecomments.js
+++ youtubecomments.js
@@ -8,11 +8,11 @@
 var DELAY = 0; // try 0, then try increasing values
 var myList = document.getElementsByClassName("dropdown-trigger style-scope ytd-menu-renderer");
 function confirmClick(callback3) {
-    document.getElementById("confirm-button").click();
+    document.getElementsByClassName("yt-simple-endpoint style-scope yt-button-renderer")[1].click();
     setTimeout(callback3, DELAY);
 }
 function itemClick(callback2, callback3)  {
-    document.getElementsByClassName("style-scope ytd-menu-navigation-item-renderer")[4].click();
+    document.getElementsByClassName("yt-simple-endpoint style-scope ytd-menu-navigation-item-renderer")[1].click();
     setTimeout(callback2, DELAY, callback3);
 }
 function listClick(element, callback1, callback2, callback3) {

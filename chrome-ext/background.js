228a0177ad04af168943fbd534e82734e9deea1b
Ian Campbell <ian.campbell87@gmail.com>
2019-01-23 01:31:50 -0500

    Change link

--- chrome-ext/background.js
+++ chrome-ext/background.js
@@ -11,7 +11,7 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: { urlContains: 'comment_history' },
+                pageUrl: { urlContains: 'www.youtube.com' },
             })],
             actions: [new chrome.declarativeContent.ShowPageAction()]
         }]);

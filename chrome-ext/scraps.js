e0cb407b82d8d235124e3ec4837af60ad873c304
Ian C <ian.campbell87@gmail.com>
2019-01-25 18:33:52 -0500

    Erase a comment

--- chrome-ext/scraps.js
+++ chrome-ext/scraps.js
@@ -1,6 +1,4 @@
 
-//each of these lines, when run one after another, deletes a comment
-
 document.getElementsByClassName("dropdown-trigger style-scope ytd-menu-renderer")[0].click(); 
 
 document.getElementsByClassName("yt-simple-endpoint style-scope ytd-menu-navigation-item-renderer")[1].click();

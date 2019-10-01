5e7897b0547debfa12c03a9db1fc2914135db4fd
Ian Campbell <ian.campbell87@gmail.com>
2019-10-01 11:41:47 -0400

    Add 2nd script for private videos

---
+++ chrome-ext/script_2.js
@@ -0,0 +1,23 @@
+var DELAY = 0; // try 0, then try increasing values
+var myList = document.getElementsByClassName("dropdown-trigger style-scope ytd-menu-renderer");
+function confirmClick(callback3) {
+    document.getElementsByClassName("style-scope yt-button-renderer style-text size-default")[1].click();
+    setTimeout(callback3, DELAY);
+}
+function itemClick(callback2, callback3)  {
+	document.getElementsByClassName("style-scope ytd-menu-navigation-item-renderer")[1].click();
+    setTimeout(callback2, DELAY, callback3);
+}
+function listClick(element, callback1, callback2, callback3) {
+    element.click();
+    setTimeout(callback1, DELAY, callback2, callback3);
+}
+function doOne(i) {
+    listClick(myList[i], itemClick, confirmClick, function() {
+        ++i;
+        if (i < myList.length) {
+            doOne(i);
+        }
+    });
+}
+doOne(0);

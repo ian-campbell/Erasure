17b3e8c50b50002005fe5f892cca8bdaf1cb5a59
Ian Campbell <ian.campbell87@gmail.com>
2019-10-01 11:04:33 -0400

    private video comment script update 1

--- chrome-ext/scraps.js
+++ chrome-ext/scraps.js
@@ -5,3 +5,19 @@ document.getElementsByClassName("yt-simple-endpoint style-scope ytd-menu-navigat
 
 document.getElementsByClassName("yt-simple-endpoint style-scope yt-button-renderer")[1].click()
 
+style-scope ytd-menu-navigation-item-renderer
+
+document.getElementsByClassName("style-scope ytd-menu-navigation-item-renderer")[1].click();
+
+document.getElementsByClassName("style-scope yt-button-renderer style-text size-default")[1].click()
+
+
+style-scope yt-button-renderer style-text size-default
+
+style-scope yt-icon-button
+
+document.getElementsByClassName("style-scope yt-icon-button circle")[0].click(); 
+
+style-scope yt-icon-button circle
+
+style-scope ytd-menu-navigation-item-renderer

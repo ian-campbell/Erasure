a6e5c9d2d8f05fca15326181b7a2d9319bcf6e52
Ian Campbell <ian.campbell87@gmail.com>
2019-10-02 11:58:36 -0400

    Combine comments and private video comments into one deletion button

--- chrome-ext/script.js
+++ chrome-ext/script.js
@@ -1,17 +1,35 @@
 var DELAY = 0; // try 0, then try increasing values
 var myList = document.getElementsByClassName("dropdown-trigger style-scope ytd-menu-renderer");
+
+
+//second delete button
 function confirmClick(callback3) {
-    document.getElementsByClassName("yt-simple-endpoint style-scope yt-button-renderer")[1].click();
+    try{
+        document.getElementsByClassName("yt-simple-endpoint style-scope yt-button-renderer")[1].click();
+    }
+    catch{
+        document.getElementsByClassName("style-scope yt-button-renderer style-text size-default")[1].click();
+    }
     setTimeout(callback3, DELAY);
 }
+
+//first delete button
 function itemClick(callback2, callback3)  {
-    document.getElementsByClassName("yt-simple-endpoint style-scope ytd-menu-navigation-item-renderer")[1].click();
+    try {
+        document.getElementsByClassName("yt-simple-endpoint style-scope ytd-menu-navigation-item-renderer")[1].click();
+    }
+    catch{
+        document.getElementsByClassName("style-scope ytd-menu-navigation-item-renderer")[1].click();
+    }
     setTimeout(callback2, DELAY, callback3);
 }
+
+//open edit/delete menu
 function listClick(element, callback1, callback2, callback3) {
     element.click();
     setTimeout(callback1, DELAY, callback2, callback3);
 }
+
 function doOne(i) {
     listClick(myList[i], itemClick, confirmClick, function() {
         ++i;
@@ -20,4 +38,5 @@ function doOne(i) {
         }
     });
 }
+
 doOne(0);

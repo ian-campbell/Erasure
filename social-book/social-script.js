fccedf1b4f69f2a2680b9235dc6178537d7d4e2f
Ian Campbell <ian.campbell87@gmail.com>
2019-10-04 14:15:23 -0400

    Better working script, removed for loop

--- social-book/social-script.js
+++ social-book/social-script.js
@@ -128,6 +128,38 @@ setTimeout(doOne(0), 3000)
 //working script
 
 
+//better working script
+function confirmClick(callback3) {
+    console.log('confirmClick activated and passed');
+    setTimeout(callback3, 0);
+}
+function itemClick(callback2, callback3)  {
+	console.log('itemclick activated and passed');
+	var d = document.getElementsByClassName("_54nc");
+	console.log('list of delete buttons made');
+	var elem = d.length - 1;
+	console.log('last element number found');
+	d[elem].click();
+	console.log('delete button clicked');
+    setTimeout(callback2, 3000, callback3);
+}
+function listClick(element, callback1, callback2, callback3) {
+    element.children[0].click();
+    console.log('listclick activated and passed');
+    setTimeout(callback1, 0, callback2, callback3);
+}
+function doOne() {
+	var myList = document.getElementsByClassName('_6a _6b uiPopover rfloat');
+	console.log('first list made')
+	listClick(myList[0], itemClick, confirmClick, function() {
+        	console.log('recursive step')
+            doOne();
+    });
+}
+setTimeout(doOne(), 3000)
+//better working script
+
+
 
 document.querySelectorAll('a[ajaxify="/allactivity/removecontent/?category_key=commentscluster&action=remove_comment&ent_identifier=S%3A_I5742869%3A2160634354040540%3A23&story_dom_id=u_4t_4&timeline_token=5742869%3A2160634354040540%3A23%3A1570053191%3A1570040366"]');
 

4da28a5d9604968251e3a86f8fd928ea9cb3cde2
Ian Campbell <ian.campbell87@gmail.com>
2019-10-07 17:54:15 -0400

    Update fbscript.js with working console script

--- chrome-ext/fbscript.js
+++ chrome-ext/fbscript.js
@@ -1,29 +1,29 @@
-//better working script
+//best working script
 function confirmClick(callback3) {
     setTimeout(callback3, 0);
 }
-function itemClick(callback2, callback3)  {
+function itemClick(callback2, i, callback3)  {
 	var d = document.getElementsByClassName("_54nc");
-	var elem = d.length - 1;
+	var elem = i;
 	d[elem].click();
     setTimeout(callback2, 3000, callback3);
 }
-function listClick(element, callback1, callback2, callback3) {
+function listClick(element, callback1, callback2, i, callback3) {
     element.children[0].click();
-    setTimeout(callback1, 0, callback2, callback3);
+    setTimeout(callback1, 0, callback2, i, callback3);
 }
-function doOne() {
+function doOne(i) {
 	var myList = document.getElementsByClassName('_6a _6b uiPopover rfloat');
-	var ll = myList.length;
 	if (myList.length < 10 ) {
 		window.scrollTo(0, document.body.scrollHeight);
 	}
-	
-	listClick(myList[0], itemClick, confirmClick, function() {
-    	console.log('recursive step')
-        doOne();
+	listClick(myList[0], itemClick, confirmClick, i, function() {
+        ++i;
+        if (i < myList.length) {
+        	console.log('recursive step')
+            doOne(i);
+        }
     });
 }
-setTimeout(doOne(), 3000)
-//better working script
-
+setTimeout(doOne(0), 3000)
+//best working script

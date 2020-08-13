9ff69fdef1ec0883b8a52ee79c934afeb0e8ebb5
Ian Campbell <ian.campbell87@gmail.com>
2020-08-13 05:55:07 -0400

    Increase PAUSE time to 8000ms

--- chrome-ext/script.js
+++ chrome-ext/script.js
@@ -4,7 +4,7 @@ var DELAY = 0;
 // if script ends but yt has more comments loading then increase this pause value.
 // this provides 1 retry attempt between list updates. useful for slow cpu/network.
 // (electronoob: 800 was the ideal value for my machine)
-var PAUSE = 0;
+var PAUSE = 8000;
 
 var myList = document.getElementsByClassName("dropdown-trigger style-scope ytd-menu-renderer");
 

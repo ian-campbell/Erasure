226a4c6f288866e7845583cd82d3c5142886fb21
Ian Campbell <ian.campbell87@gmail.com>
2020-10-01 05:18:35 -0400

    Add live chat deletion functionality and buttons

--- chrome-ext/popup.js
+++ chrome-ext/popup.js
@@ -1,10 +1,14 @@
 let changeColor = document.getElementById('deleteComments');
 let navigate = document.getElementById('navigate');
+let deleteComments = document.getElementById('deleteComments');
+let deleteLiveChats = document.getElementById('deleteLiveChats');
+let liveChats = document.getElementById('liveChats');
 
 chrome.storage.sync.get('color', function(data) {
     changeColor.style.backgroundColor = data.color;
     changeColor.setAttribute('value', data.color);
 });
+
 
 
 //relevant code 
@@ -21,4 +25,15 @@ navigate.onclick = function(element) {
     });
 };
 
- 
+liveChats.onclick = function(element) {
+    chrome.tabs.query({ active: true, currentWindow: true}, function(tabs){
+        chrome.tabs.update(tabs[0].id, {url:'https://www.youtube.com/feed/history/live_chat_history'})
+    });
+};
+
+deleteLiveChats.onclick = function(element) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        chrome.tabs.executeScript(
+            tabs[0].id, { file: 'live_chats.js' });
+    });
+};

e4716b957194676bb2e7cea6d30826b4d4ec8cbb
Ian Campbell <ian.campbell87@gmail.com>
2020-10-21 08:36:53 -0400

    Separate statements

--- chrome-ext/discussion.js
+++ chrome-ext/discussion.js
@@ -8,3 +8,11 @@ var PAUSE = 8000;
 
 var myList = document.getElementsByClassName("dropdown-trigger style-scope ytd-menu-renderer");
 
+
+// delete button for owner comments
+document.getElementsByClassName("yt-simple-endpoint style-scope ytd-menu-navigation-item-renderer")[1].click();
+
+
+// delete button for others' comments
+document.getElementsByClassName("style-scope ytd-menu-popup-renderer")[1].click();
+document.getElementsByClassName("yt-simple-endpoint style-scope yt-button-renderer")[1].click();

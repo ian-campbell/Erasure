dc798df54bb93cdca67f3bfd73c36caf72c7bfa9
Ian Campbell <ian.campbell87@gmail.com>
2020-10-26 09:13:53 -0400

    collapsible

--- chrome-ext/popup.js
+++ chrome-ext/popup.js
@@ -56,10 +56,10 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
     } else {
-      content.style.display = "block";
+      content.style.maxHeight = content.scrollHeight + "px";
     }
   });
 }

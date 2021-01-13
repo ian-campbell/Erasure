91117879ad8ad9faff4df7f5c595ba19f843adc1
Ian Campbell <ian.campbell87@gmail.com>
2021-01-13 04:02:02 -0500

    Add first click function

--- chrome-ext/fb.js
+++ chrome-ext/fb.js
@@ -2,4 +2,16 @@ var g = document.getElementsByClassName("sp_k2A2bGWLzV9_3x")
 g[15].click();
 $(".sp_d1AIRL-Hywn_3x").click()
 
+clik = () => {
+    $(".sp_d1AIRL-Hywn_3x").click();
+}
+
+myfunc = (data, myfunc2) => {
+    var myList = data;
+    for (var i=15; i < myList.length; i++) {
+        myList[i].click()
+        setTimeout(myfunc2, 1000); 
+    }
+}
+
 

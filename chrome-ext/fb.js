998a6470ccfe1fe198e5221f8affee55e69c7b2e
Ian Campbell <ian.campbell87@gmail.com>
2021-01-13 13:59:49 -0500

    Script works tentatively on fb new layout

--- chrome-ext/fb.js
+++ chrome-ext/fb.js
@@ -1,17 +1,47 @@
 var g = document.getElementsByClassName("sp_k2A2bGWLzV9_3x")
-g[15].click();
+g[4].click();
 $(".sp_d1AIRL-Hywn_3x").click()
 
+const PAUSE = 2000;
+
 clik = () => {
-    $(".sp_d1AIRL-Hywn_3x").click();
+    let g = document.getElementsByClassName("sp_d1AIRL-Hywn_3x");
+    g[0].click();
 }
 
-myfunc = (data) => {
-    var myList = data;
-    for (var i=15; i < myList.length; i++) {
-        myList[i].click()
-        setTimeout($(".sp_d1AIRL-Hywn_3x").click(), 1000); 
+getList = () => {
+    let g = document.getElementsByClassName("sp_k2A2bGWLzV9_3x");
+    return g;
+}
+
+myfunc = () => {
+    if (commentsAvailable()){
+        var myList = getList();
+        myList[5].click();
+        setTimeout(()=>{
+            clik();
+        }, PAUSE);
+        if (myList.length > 5){
+            setTimeout(()=>{
+                myfunc();
+            }, PAUSE);
+        }
+        else{ console.log("erasure: attempting to retry in 1000 ms");
+            setTimeout(()=>{
+                myfunc();
+            },PAUSE);
+        }
     }
 }
 
 
+//check for available comments
+commentsAvailable = () => {
+    let comments = document.getElementsByClassName("sp_k2A2bGWLzV9_3x");
+    if (comments.length > 6){
+        return true;
+    }
+    else {
+        return false;
+    }
+}

9db593203ec19a422c212f12b7444df3ddee875d
Ian Campbell <ian.campbell87@gmail.com>
2021-01-13 15:34:29 -0500

    Doesn't work, FB uses dynamic classes. Pasted some snippets

--- chrome-ext/fb.js
+++ chrome-ext/fb.js
@@ -1,23 +1,56 @@
 var g = document.getElementsByClassName("sp_k2A2bGWLzV9_3x")
 g[4].click();
 $(".sp_d1AIRL-Hywn_3x").click()
+document.getElementsByClassName('sp_ImHyMYe7JWT_3x')
+//parent container?
+document.getElementsByClassName('aov4n071')
 
-const PAUSE = 2000;
+var m = document.querySelectorAll('[aria-label="Action options"]');
+var s = document.querySelectorAll('[data-pagelet="root"]');
 
+var t = Array.from(document.querySelectorAll('div'))
+  .find(el => el.textContent === 'Activity Log');
+
+
+sortByComments = () => {
+
+    let h = document.getElementsByClassName('emxnvquj ni8dbmo4 tr9rh885 ciko2aqh');
+    let n = h[0].querySelectorAll('*[data-visualcompletion]');
+    let a = n[20];
+    let v = a.querySelectorAll("[class='bp9cbjyn j83agx80 btwxx1t3']");
+    v[0].click();
+    let save = document.querySelectorAll("[aria-label='Save Changes']");
+    save[0].click();
+    setTimeout(() => {
+        myfunc();
+    }, 5000)
+}
+
+openFilter = () => {
+    var b = document.getElementsByClassName('j83agx80 pfnyh3mw');
+    b[23].firstElementChild.click();
+    setTimeout(() =>{
+        sortByComments();
+    }, 8000)
+}
+
+
+const PAUSE = 1000;
+var starter = document.getElementsByClassName('sp_ImHyMYe7JWT_3x');
 clik = () => {
     let g = document.getElementsByClassName("sp_d1AIRL-Hywn_3x");
     g[0].click();
 }
 
 getList = () => {
-    let g = document.getElementsByClassName("sp_k2A2bGWLzV9_3x");
-    return g;
+    let n = document.getElementsByClassName("sp_k2A2bGWLzV9_3x");
+    return n;
 }
 
 myfunc = () => {
     if (commentsAvailable()){
         var myList = getList();
-        myList[5].click();
+        myList[4].click();
         setTimeout(()=>{
             clik();
         }, PAUSE);
@@ -38,7 +71,7 @@ myfunc = () => {
 //check for available comments
 commentsAvailable = () => {
     let comments = document.getElementsByClassName("sp_k2A2bGWLzV9_3x");
-    if (comments.length > 6){
+    if (comments.length > 5){
         return true;
     }
     else {

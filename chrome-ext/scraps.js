ff2b335b24b6134f4f82d651faee5b17aa0b3905
ian <ian.campbell87@gmail.com>
2021-02-06 07:34:03 -0500

    Add scraps.js to store old code for quick reference

---
+++ chrome-ext/scraps.js
@@ -0,0 +1,73 @@
+// This is the old script before the YouTube comment page was changed.
+// Keeping it for reference
+
+
+
+// try 0, then try increasing values. this is delay between comment deletions
+var DELAY = 0;
+
+// if script ends but yt has more comments loading then increase this pause value.
+// this provides 1 retry attempt between list updates. useful for slow cpu/network.
+// (electronoob: 800 was the ideal value for my machine)
+var PAUSE = 1000;
+
+var myList = document.getElementsByClassName("dropdown-trigger style-scope ytd-menu-renderer");
+
+
+//second delete button
+function confirmClick(callback3) {
+    try{
+        document.getElementsByClassName("yt-simple-endpoint style-scope yt-button-renderer")[1].click();
+    }
+    catch{
+        document.getElementsByClassName("style-scope yt-button-renderer style-text size-default")[1].click();
+    }
+    setTimeout(callback3, DELAY);
+}
+
+//first delete button
+function itemClick(callback2, callback3)  {
+    try {
+        document.getElementsByClassName("yt-simple-endpoint style-scope ytd-menu-navigation-item-renderer")[1].click();
+    }
+    catch{
+        document.getElementsByClassName("style-scope ytd-menu-navigation-item-renderer")[1].click();
+    }
+    setTimeout(callback2, DELAY, callback3);
+}
+
+//open edit/delete menu
+function listClick(element, callback1, callback2, callback3) {
+    element.click();
+    setTimeout(callback1, DELAY, callback2, callback3);
+}
+
+//check for available comments
+function commentsAvailable () {
+    for(x of document.getElementsByTagName("ytd-comment-history-entry-renderer")) {
+        if(x.getAttribute("is-dismissed") == null) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function doOne(i) {
+    if(commentsAvailable()) {
+        listClick(myList[i], itemClick, confirmClick, function() {
+            ++i;
+            if (i < myList.length) {
+                doOne(i);
+            }else {
+                console.log("erasure: attempting to retry in %s ms",PAUSE);
+                setTimeout(()=>{
+                    doOne(0);
+                },PAUSE);
+            }
+        });
+    } else {
+        console.log("erasure: there are no comments, exiting.");
+    }
+}
+
+doOne(0);

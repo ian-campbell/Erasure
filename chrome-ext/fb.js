1b3b9ea234d177b1cea24f7fbf857052fe2f7105
ian <ian.campbell87@gmail.com>
2021-02-26 21:11:14 -0500

    Add code to simulate a mouseover event to reveal a div

--- chrome-ext/fb.js
+++ chrome-ext/fb.js
@@ -98,3 +98,22 @@ commentsAvailable = () => {
         return false;
     }
 }
+
+
+// messenger unsend script
+let p = document.getElementsByClassName("oajrlxb2 gs1a9yip g5ia77u1 mtkw9kbi tlpljxtp qensuy8j ppp5ayq2 goun2846 ccm00jje s44p3ltw mk2mc5f4 rt8b4zig n8ej3o3l agehan2d sk4xxmp2 rq0escxv nhd2j8a9 j83agx80 mg4g778l btwxx1t3 pfnyh3mw p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x tgvbjcpo hpfvmrgz jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso l9j0dhe7 i1ao9s8h esuyzwwr f1sip0of du4w35lb lzcic4wl abiwlrkh p8dawk7l mkd47r93 rgmg9uty");
+let x = document.getElementsByClassName("p1ueia1e pgctjfs5 l8rlqa9s sh06z9xi i2p6rm4e");
+x[x.length-2]
+// x
+var element = document.getElementById('name');
+element.addEventListener('mouseover', function() {
+  console.log('Event triggered');
+});
+
+var event = new MouseEvent('mouseover', {
+  'view': window,
+  'bubbles': true,
+  'cancelable': true
+});
+
+element.dispatchEvent(event);

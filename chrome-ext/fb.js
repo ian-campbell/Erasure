ba9a96e016ea34d5f8c20b03add90c7775b2d8de
ian-campbell <ian.campbell87@gmail.com>
2021-05-27 23:59:36 -0400

    Fix indentation in fb.js

--- chrome-ext/fb.js
+++ chrome-ext/fb.js
@@ -7,75 +7,75 @@ Then paste this script into your javascript console and run it.
 const PAUSE = 2000;
 const RELOAD = 5000;
 clik = () => {
-    var s = document.querySelectorAll('[data-pagelet="root"]');
-    try{
-        s[2].getElementsByTagName('i')[0].click();
-    }
-    catch{
-        setTimeout(()=>{
-            s[2].getElementsByTagName('i')[0].click();
-        }, 1500)
-    }   
+  var s = document.querySelectorAll('[data-pagelet="root"]');
+  try{
+    s[2].getElementsByTagName('i')[0].click();
+  }
+  catch{
+    setTimeout(()=>{
+      s[2].getElementsByTagName('i')[0].click();
+    }, 1500)
+  }   
 }
 
 // creates a list of all comments on the screen
 getList = () => {
-    var n = document.querySelectorAll('[aria-label="Action options"]');
-    return n;
+  var n = document.querySelectorAll('[aria-label="Action options"]');
+  return n;
 }
 
 queryErrorCheck = () => {
-    let e = document.querySelectorAll('[aria-label="Query Error"]');
-    if (e.length >= 1){
-        return true;
-    }
-    else{
-        return false;
-    }
+  let e = document.querySelectorAll('[aria-label="Query Error"]');
+  if (e.length >= 1){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 //check for available comments
 commentsAvailable = () => {
-    let comments = document.querySelectorAll('[aria-label="Action options"]');
-    if (comments.length >= 1){
-        return true;
-    }
-    else {
-        return false;
-    }
+  let comments = document.querySelectorAll('[aria-label="Action options"]');
+  if (comments.length >= 1){
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 // main function
 facebookCommentDelete = (i) => {
-    // handle an error box
-    if (queryErrorCheck()){
-        let r = document.querySelectorAll('[aria-label="OK"]');
-        r[0].click();
-        i = i + 1;
-        setTimeout(() => {
-            facebookCommentDelete(i);
-        }, 5000)
+  // handle an error box
+  if (queryErrorCheck()){
+    let r = document.querySelectorAll('[aria-label="OK"]');
+    r[0].click();
+    i = i + 1;
+    setTimeout(() => {
+      facebookCommentDelete(i);
+    }, 5000)
+  }
+  if (commentsAvailable()){
+    var myList = getList();
+    myList[i].click();
+    setTimeout(()=>{
+      clik();  // click the delete button
+    }, PAUSE);
+    if (myList.length > 0){
+      setTimeout(()=>{
+        facebookCommentDelete(i); // recursive call
+      }, PAUSE);
     }
-    if (commentsAvailable()){
-        var myList = getList();
-        myList[i].click();
-        setTimeout(()=>{
-            clik();  // click the delete button
-        }, PAUSE);
-        if (myList.length > 0){
-            setTimeout(()=>{
-                facebookCommentDelete(i); // recursive call
-            }, PAUSE);
-        }
-        else{ console.log("erasure: attempting to retry in 3000 ms");
-            setTimeout(()=>{
-                facebookCommentDelete(i); // recursive call after waiting
-            },RELOAD);
-        }
+    else{ console.log("erasure: attempting to retry in 3000 ms");
+      setTimeout(()=>{
+        facebookCommentDelete(i); // recursive call after waiting
+      },RELOAD);
     }
-    else{ 
-        console.log("Erasure completed, exiting.");
-    }
+  }
+  else{ 
+    console.log("Erasure completed, exiting.");
+  }
 }
 
 // try starting at 0, if it doesn't work try a bigger number

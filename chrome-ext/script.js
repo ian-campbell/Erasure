9f32e960ad27fc2b9a41fb687ac5190261346ac5
ian-campbell <ian.campbell87@gmail.com>
2021-05-27 23:55:36 -0400

    Fix indentation in script.js

--- chrome-ext/script.js
+++ chrome-ext/script.js
@@ -10,50 +10,50 @@ var myList = document.getElementsByClassName("YxbmAc");
 
 // scroll to the bottom of the page
 function autoScroll () {
-    window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" });
+  window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" });
 }
 
 // check for available comments
 function commentsAvailable2 () {
-    var m = document.getElementsByClassName("YxbmAc");
-    if(m.length > 0){
-        console.log('erasure: %s comments are available.', m.length);
-        return true;
-    }
-    return false;
+  var m = document.getElementsByClassName("YxbmAc");
+  if(m.length > 0){
+    console.log('erasure: %s comments are available.', m.length);
+    return true;
+  }
+  return false;
 }
 
 /* This function clicks the delete button for both regular and 
    non-G-suite account users. */
 function deleteClick(element, callback1){
-    try{ 
-        element.querySelectorAll(".VfPpkd-rymPhb-pZXsl")[1].click();
-        setTimeout(callback1, DELAY);
-    }
-    catch{
-        element.querySelectorAll(".VfPpkd-Bz112c-LgbsSe")[0].click();
-        setTimeout(callback1, DELAY);
-    }
+  try{ 
+    element.querySelectorAll(".VfPpkd-rymPhb-pZXsl")[1].click();
+    setTimeout(callback1, DELAY);
+  }
+  catch{
+    element.querySelectorAll(".VfPpkd-Bz112c-LgbsSe")[0].click();
+    setTimeout(callback1, DELAY);
+  }
 }
 
 /* Main function */
 function main(i) {
-    if(commentsAvailable2()) {
-        deleteClick(myList[i], () => {
-            ++i;
-            if (i < myList.length) {
-                main(i);
-            }else {
-                console.log("erasure: attempting to retry in %s ms",PAUSE);
-                autoScroll();
-                setTimeout(()=>{
-                    main(0);
-                },PAUSE);
-            }
-        });
-    } else {
-        console.log("erasure: there are no comments, exiting.");
-    }
+  if(commentsAvailable2()) {
+    deleteClick(myList[i], () => {
+      ++i;
+      if (i < myList.length) {
+        main(i);
+      }else {
+        console.log("erasure: attempting to retry in %s ms",PAUSE);
+        autoScroll();
+        setTimeout(()=>{
+          main(0);
+        },PAUSE);
+      }
+    });
+  } else {
+    console.log("erasure: there are no comments, exiting.");
+  }
 }
 
 main(0);
